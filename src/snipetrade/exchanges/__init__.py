f0162f631ffe48270b583a9ac9cb93cedd4ac399
NobleWolf412 <maccardi4431@gmail.com>
2025-11-10 22:39:22 -0500

    Refactor exchange layer with CCXT adapter

--- src/snipetrade/exchanges/__init__.py
+++ src/snipetrade/exchanges/__init__.py
@@ -1,1 +1,16 @@
-"""Exchange connector modules for multi-exchange support"""
+"""Exchange connector modules for multi-exchange support."""
+
+from .base import Exchange, ExchangeError, OHLCV, RateLimitError
+from .ccxt_adapter import CCXTExchangeAdapter, create_exchange
+from .phemex_checker import is_pair_on_phemex
+
+__all__ = [
+    "Exchange",
+    "ExchangeError",
+    "RateLimitError",
+    "OHLCV",
+    "CCXTExchangeAdapter",
+    "create_exchange",
+    "is_pair_on_phemex",
+]
+
